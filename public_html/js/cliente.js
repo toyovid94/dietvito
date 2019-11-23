09294c41c558393e1f12d567cf95e742168ffc1f
Jon <johannaallendesanz@hotmail.com>
2019-11-23 22:15:22 +0100

    Completado HTML y JS basico de la pagina, añadido fondo, cambiado color de la barra

--- public_html/js/cliente.js
+++ public_html/js/cliente.js
@@ -1,1 +1,13 @@
-
+function mostrar() {
+    var x = document.getElementById("seleccionConsulta");
+    var valor = x.options[x.selectedIndex].value;
+    document.getElementById("nada").style.display = "none";
+    document.getElementById("peso").style.display = "none";
+    document.getElementById("regAct").style.display = "none";
+    document.getElementById("vistaTablaAct").style.display = "none";
+    document.getElementById("vistaActReal").style.display = "none";
+    document.getElementById("tablaAlim").style.display = "none";
+    document.getElementById("regAlim").style.display = "none";
+    document.getElementById("alimConsum").style.display = "none";
+    document.getElementById(valor).style.display = "block";
+}
